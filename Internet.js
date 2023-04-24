429afe9ee24c7fe8985da01b5486dbc73a139714
Aurora Wu(吳映慈) <Aurora_Wu@corpnet.asus>
2023-04-24 11:23:27 +0800

    qmei1

---
+++ Internet.js
@@ -0,0 +1,420 @@
+const data = [
+    {
+        id: 1,
+        title: "阿梅的生日是",
+        option: ["11/22", "11/26", "11/28", "11/29"],
+        answer: "B"
+    },
+    {
+        id: 2,
+        title: "Q家族貴賓狗,不包括:",
+        option: ["Q妞", "Q比", "Q寶", "Q新"],
+        answer: "D"
+    },
+    {
+        id: 3,
+        title: "使用IG到此帳號qmeimei20170307,你可以查看",
+        option: ["彰化媽祖婆", "吳梅菁", "沈沁玹", "以上皆是"],
+        answer: "D"
+    },
+    {
+        id: 4,
+        title: "關於仁仁的敘述何者錯誤",
+        option: ["是個律師", "深患殘疾", "差一分上台大", "外號'Q仁'"],
+        answer: "A"
+    },
+    {
+        id: 5,
+        title: "有先見之明離家出走的狗,是?",
+        option: ["Q妞", "Q希", "Q寶", "Q咘"],
+        answer: "A"
+    },
+    {
+        id: 6,
+        title: "阿梅會在7-11做甚麼?",
+        option: ["摸摸", "買阿華田", "請人去整形", "以上皆是"],
+        answer: "D"
+    },
+    {
+        id: 7,
+        title: "下列何者未與阿梅交往過?",
+        option: ["小夜", "小武", "小慶", "賴小小"],
+        answer: "C"
+    },
+    {
+        id: 8,
+        title: "下列何事件時間線最早?",
+        option: ["愛上表叔", "貝殼內衣啞巴吹", "和美里事件", "跟小武結婚"],
+        answer: "A"
+    },
+    {
+        id: 9,
+        title: "阿梅的戶頭剩多少錢?(再領普前)",
+        option: ["158", "258", "358", "600"],
+        answer: "B"
+    },
+    {
+        id: 10,
+        title: "阿梅某年生日派對辦在?",
+        option: ["築間", "建國路9號", "悅河精品旅館", "KTV"],
+        answer: "C",
+    },
+    {
+        id: 11,
+        title: "147的外號何者有誤",
+        option: ["普信龍", "親家母", "漂亮公主", "芯喵喵的女兒"],
+        answer: "C",
+    },
+    {
+        id: 12,
+        title: "147號稱體重?",
+        option: ["55", "100", "147", "80"],
+        answer: "A",
+    },
+    {
+        id: 13,
+        title: "梅宇宙中，下列何者學歷最高",
+        option: ["賴小小", "金樽維(小)", "政喻", "紹新"],
+        answer: "C",
+    },
+    {
+        id: 14,
+        title: "哪一個不是呂紹新的暱稱?",
+        option: ["越南寶特瓶", "射匯人", "梅有想睡", "新寶"],
+        answer: "D",
+    },
+    {
+        id: 15,
+        title: "阿梅的皮草多少錢",
+        option: ["1000", "1280", "12800", "399"],
+        answer: "C",
+    },
+    {
+        id: 16,
+        title: "小夜養的狗是?",
+        option: ["柴犬", "法鬥", "臘腸", "柯基"],
+        answer: "D",
+    },
+    {
+        id: 17,
+        title: "某姊妹回鍋離婚群時用哪句話釣出淺水阿梅",
+        option: ["幹破你娘", "寵物殺手", "你出來我1禮拜不罵你", "她又到處睡"],
+        answer: "D",
+    },
+    {
+        id: 18,
+        title: "下列哪位ㄓㄣ沒有出現過",
+        option: ["溱", "蓁", "榛", "臻"],
+        answer: "A"
+    },
+    {
+        id: 19,
+        title: "芯喵喵的口頭禪是",
+        option: ["心疼可以贊助", "我要去工作了", "#不是免費", "可以不要看"],
+        answer: "B"
+    },
+    {
+        id: 20,
+        title: "阿梅做過的工作下列何者錯誤",
+        option: ["兔兔小舖老闆娘", "酒店", "小護士", "經紀"],
+        answer: "C"
+    },
+    {
+        id: 21,
+        title: "阿梅就讀哪個大學",
+        option: ["建國", "大葉", "彰師", "梅讀大學"],
+        answer: "A"
+    },
+    {
+        id: 22,
+        title: "誰是禿頭",
+        option: ["小武", "小夜", "小慶", "房東"],
+        answer: "D"
+    },
+    {
+        id: 23,
+        title: "哪隻狗最邪氣",
+        option: ["Q咩", "Q妞", "Q寶", "Q希"],
+        answer: "A"
+    },
+    {
+        id: 24,
+        title: "最寵粉的人,是",
+        option: ["Q新", "147", "賴小小", "小夜"],
+        answer: "D"
+    },
+    {
+        id: 25,
+        title: "哪一個群組送了阿梅花籃",
+        option: ["離婚群", "悅河群", "蛇群", "兔兔粉絲團"],
+        answer: "B"
+    },
+    {
+        id: 26,
+        title: "阿梅想要去哪個國家就讀碩士",
+        option: ["加拿大", "日本", "澳洲", "紐西蘭"],
+        answer: "D"
+    },
+    {
+        id: 27,
+        title: "阿梅買了甚麼去給小武探監並請求復婚",
+        option: ["麥當勞", "熱炒", "會客菜", "肯德基"],
+        answer: "A"
+    },
+    {
+        id: 28,
+        title: "小武入獄服刑前幾公斤?",
+        option: ["80", "60", "61", "90"],
+        answer: "C"
+    },
+    {
+        id: 29,
+        title: "阿梅稱小武何時出獄",
+        option: ["2026", "2025", "2028", "2036"],
+        answer: "A"
+    },
+    {
+        id: 30,
+        title: "慈慈幾歲生小孩",
+        option: ["13", "14", "12", "11"],
+        answer: "A"
+    },
+    {
+        id: 31,
+        title: "慈慈的小孩叫?",
+        option: ["圓圓", "球球", "寶寶", "小寶"],
+        answer: "B"
+    },
+    {
+        id: 32,
+        title: "慈慈曾經用甚麼在路口爆打他人",
+        option: ["拳頭", "水壺", "安全帽", "包包"],
+        answer: "C"
+    },
+    {
+        id: 33,
+        title: "阿梅最主要的工作是?",
+        option: ["獵奶狗", "跟Q仁要錢", "在IG發一堆語錄", "以上皆是"],
+        answer: "D"
+    },
+    {
+        id: 34,
+        title: "那一個不是梅宇宙的特色?",
+        option: ["都是8+9", "男人來的快去也快", "每次都會改過自新", "女生通常都會反目成仇"],
+        answer: "C"
+    },
+    {
+        id: 35,
+        title: "籽魚曾經的男友是哪位?",
+        option: ["哈麥二齒", "賀哥", "邱比特", "bkk"],
+        answer: "A"
+    },
+    {
+        id: 36,
+        title: "梅菁曾用甚麼東西來自殘",
+        option: ["沐浴乳", "充電線", "咖啡包", "以上皆是"],
+        answer: "D"
+    },
+    {
+        id: 37,
+        title: "小武的媽媽是?",
+        option: ["胡佳琳", "胡佳琦", "潘信孝", "胡心婷"],
+        answer: "B"
+    },
+    {
+        id: 38,
+        title: "阿梅沒有養過哪種動物?",
+        option: ["貓", "狗", "老鼠", "鳥"],
+        answer: "D"
+    },
+    {
+        id: 39,
+        title: "147被留下爆龍，超爆龍的評論是在?",
+        option: ["寧夏夜市", "士林夜市", "中壢夜市", "饒河夜市"],
+        answer: "B"
+    },
+    {
+        id: 40,
+        title: "下列何者不是阿梅與小夜時期發生的故事?",
+        option: ["十包打底", "騙朋友喝咖啡", "扛點滴逃跑", "喝沐浴乳"],
+        answer: "B"
+    },
+    {
+        id: 41,
+        title: "畢旅妹跟誰發生了牙刷事件?",
+        option: ["熊", "賀哥", "假韓", "富善宇"],
+        answer: "C"
+    },
+    {
+        id: 42,
+        title: "甚麼東西要分開算",
+        option: ["陽春麵", "金莎", "阿華田", "肉圓"],
+        answer: "A"
+    },
+    {
+        id: 43,
+        title: "阿梅不會出現在?",
+        option: ["紐西蘭", "夜店", "建國學餐", "監獄"],
+        answer: "A"
+    },
+    {
+        id: 44,
+        title: "阿梅喜歡的類型",
+        option: ["奶狗", "8+9", "有爸爸感覺的人", "以上皆是"],
+        answer: "D"
+    },
+    {
+        id: 45,
+        title: "阿梅復婚不成做了甚麼事",
+        option: ["燒假的結婚證書", "踢燒金紙的桶子", "跑去夜店釣奶狗", "以上皆是"],
+        answer: "D"
+    },
+    {
+        id: 46,
+        title: "阿梅有對Q新說過以下哪句話?",
+        option: ["黑死!外翻!", "先去整形好不好", "長那醜逼樣", "拍謝 我有D"],
+        answer: "A"
+    },
+    {
+        id: 47,
+        title: "阿梅自稱自己有",
+        option: ["C", "B", "D", "E"],
+        answer: "C"
+    },
+    {
+        id: 48,
+        title: "阿梅曾經拿了一疊A4紙到警局告那裡的吃瓜姊妹?",
+        option: ["離婚群", "聖母梅群", "Dcard", "麻糬群"],
+        answer: "C"
+    },
+    {
+        id: 49,
+        title: "147號稱有超大粉色器官且有割包皮的人是",
+        option: ["羽球男", "雙魚男", "阿輝", "阿堂"],
+        answer: "D"
+    },
+    {
+        id: 50,
+        title: "阿梅的第一個刺青是",
+        option: ["HELLO KITTY", "大耳狗", "Q妞", "布丁狗"],
+        answer: "A"
+    },
+];
+const topic_exist = new Array(data.length).fill(false);
+const topic = new Array();
+
+
+
+const SectionTopic = document.querySelector(".inner-section");
+
+
+const SetTopic = ()=>{
+    for (var i = 0; i < 25; i++) {
+        let n = Math.floor((Math.random() * 50) + 1);
+        while (topic_exist[n] == true) {
+            n = Math.floor((Math.random() * 50) + 1);
+        }
+    
+        topic_exist[n] = true;
+        topic.push(data[n]);
+    }
+}
+
+
+
+
+let count = 0;
+let grade = 0;
+
+
+const ReTopic = () => {
+    SectionTopic.innerHTML = `
+        <h2 class="title">${topic[count].id}.${topic[count].title}</h2>
+        <button class="option A">(A) ${topic[count].option[0]}</button>
+        <button class="option B">(B) ${topic[count].option[1]}</button>
+        <button class="option C">(C) ${topic[count].option[2]}</button>
+        <button class="option D">(D) ${topic[count].option[3]}</button>
+        <h3 class="grade">共 25 題,目前做到第 1 題,總共得分:0</h3>`;
+};
+
+const fix = document.querySelector('.fail');
+const fix_content = document.querySelector('.fail-content');
+const fix_title = document.querySelector('.fail-title');
+
+
+const FixAnswer = (b)=>{
+    if(b==true){
+        fix.textContent = "";
+        fix_content.textContent = "";
+        fix_title.textContent="";
+    }
+    else{
+        fix_title.textContent="答錯了!";
+        fix.textContent = topic[count].id + "." + topic[count].title;
+        let correctAnswer = 0;
+        if(topic[count].answer=="B"){
+            correctAnswer = 1;
+        }
+        else if(topic[count].answer=="C"){
+            correctAnswer = 2;
+        }
+        else if(topic[count].answer=="D"){
+            correctAnswer = 3;
+        }
+        fix_content.textContent = "正確答案為 : " + topic[count].option[correctAnswer];
+    }
+};
+
+
+SetTopic();
+ReTopic();
+
+const YourAnswer = document.querySelectorAll('.option');
+const Title = document.querySelector('.title');
+const YourGrade = document.querySelector('.grade');
+const endBtn = document.querySelector('.end');
+
+
+
+YourAnswer.forEach((answer) => {
+    answer.addEventListener('click', (e) => {
+        console.log('123');
+        if (e.currentTarget.classList.contains(topic[count].answer)) {
+            grade += 4;
+            FixAnswer(true);
+        }
+        else{
+            FixAnswer(false);
+        }
+        count++;
+        if (count < 25) {
+            YourGrade.textContent = "共 25 題,目前做到第 " + (count + 1) + "題,總共得分:" + grade;
+            Title.textContent = topic[count].id + "." + topic[count].title;
+            YourAnswer.forEach((item, index) => {
+                if (index == 0) {
+                    item.textContent = "(A) " + topic[count].option[index];
+                }
+                if (index == 1) {
+                    item.textContent = "(B) " + topic[count].option[index];
+                }
+                if (index == 2) {
+                    item.textContent = "(C) " + topic[count].option[index];
+                }
+                if (index == 3) {
+                    item.textContent = "(D) " + topic[count].option[index];
+                }
+
+            });
+        }
+        else {
+            YourGrade.textContent = "結束了! 你得到"+grade+"分";
+            endBtn.style.display = 'inline-block';
+        }
+
+    });
+});
+
+
+endBtn.addEventListener('click',()=>{
+    window.location.reload();
+});
